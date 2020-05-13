bb509b1d5fce3a9c128155f88d8bbaf2f598f43c
Amon.Sabul <Amon.Sabul@BREFTLPT0012.CRAFTSILICON.LOCAL>
2020-05-13 22:34:15 +0300

    A game of darts, a function(let us call it score) that given a point in the target (defined by its real cartesian coordinates x and y), returns the correct amount earned by a dart landing in that point

---
+++ src/darts/Darts.java
@@ -0,0 +1,44 @@
+package darts;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Darts {
+
+    private double result;
+
+    int score(double x, double y) {
+        result = (x * x) + (y * y);
+        if (result <= 1) {
+            return 10;
+        } else if (result <= 25) {
+            return 5;
+        } else if (result <= 100) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void main() {
+        try {
+            Boolean end = false;
+            while (end == false) {
+                Darts dart = new Darts();
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("X cordinate of the target:");
+                double x = br.read();
+                System.out.println("Y cordinate of the target:");
+                double y = br.read();
+                System.out.println("Your score:");
+                dart.score(x, y);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Darts.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("We could not find your score, please play again");
+        }
+
+    }
+}
